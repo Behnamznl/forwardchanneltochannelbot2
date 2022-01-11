81e742bd79e12096eb4a198d7de950a1e93dc81d
Behnamznl <66165821+Behnamznl@users.noreply.github.com>
2022-01-12 02:23:28 +0330

    Update forwardchanneltochannelbot.py

--- plugins/forwardchanneltochannelbot.py
+++ plugins/forwardchanneltochannelbot.py
@@ -6,7 +6,7 @@ async def fctc(client, message):
           try:
                 await client.forward_messages(
                     chat_id='-1001685148429',
-                    from_chat_id='@divar_daneshjooyan',
+                    from_chat_id='-1001475270628',
                     message_ids=message.forward_from_message_id
                 )
                 message.forward_from_message_id = message.forward_from_message_id + 1
