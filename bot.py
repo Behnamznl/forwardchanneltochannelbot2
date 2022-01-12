335696475014c24b9c5b9420326f322191dc4d9e
Behnamznl <66165821+Behnamznl@users.noreply.github.com>
2022-01-13 01:25:06 +0330

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,15 @@
+from pyrogram import *
+
+api_id = '17683843'
+api_hash = '45865a99da5a1296879e48ca89801759'
+
+bot_tok = '5053121664:AAGnzffX4z5AS7306_A3pRbCNm9UL59e3Pw'
+
+plugins = dict(root='plugins')
+
+app = Client('forwardchanneltochannelbot1', api_id, api_hash, bot_token=bot_tok, plugins=plugins)
+
+try:
+    app.run()
+except KeyboardInterrupt:
+    print("Exited.")
