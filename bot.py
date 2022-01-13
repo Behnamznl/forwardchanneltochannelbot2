ddf803e44bb5f4b4372225e8738c6ccdc8aa6788
Behnamznl <66165821+Behnamznl@users.noreply.github.com>
2022-01-13 22:40:08 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ api_hash = '45865a99da5a1296879e48ca89801759'
 
 bot_tok = '5053121664:AAGnzffX4z5AS7306_A3pRbCNm9UL59e3Pw'
 
-plugins = dict(root='thebesttranslatebot')
+plugins = dict(root='plugins')
 
 app = Client('forwardchanneltochannelbot1', api_id, api_hash, bot_token=bot_tok, plugins=plugins)
 
