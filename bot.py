9fa3258e640c963fd46aef97e6cc24129317c818
Behnamznl <66165821+Behnamznl@users.noreply.github.com>
2022-01-13 22:10:09 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,16 @@
 from pyrogram import *
 
+#
+
+
 api_id = '17683843'
 api_hash = '45865a99da5a1296879e48ca89801759'
 
 bot_tok = '5053121664:AAGnzffX4z5AS7306_A3pRbCNm9UL59e3Pw'
 
+plugins = dict(root='thebesttranslatebot')
 
-app = Client('forwardchanneltochannelbot', api_id, api_hash, bot_token=bot_tok)
-
-@Client.on_message(filters.forwarded)
-async def idd23(client4, message4):
-    message4.text = message4.text.split('@Divar_Daneshjooyan')[0]
-    message4.text = message4.text + str('@DarkhasthayDaneshjo')
-    await client4.send_message(chat_id='-1001685148429', text=message4.text)
-
+app = Client('forwardchanneltochannelbot1', api_id, api_hash, bot_token=bot_tok, plugins=plugins)
 
 try:
     app.run()
